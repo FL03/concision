1844956405c292052cf9a121f38bd2f41f31d338
FL03 <jo3mccain@icloud.com>
2022-07-03 16:49:46 -0500

    Update

---
+++ concision/src/core/linalg.rs
@@ -0,0 +1,20 @@
+/*
+    Appellation: linalg
+    Context:
+    Creator: FL03 <jo3mccain@icloud.com>
+    Description:
+        ... Summary ...
+ */
+
+pub trait MatrixSpec: Sized {
+    type Data;
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn simple() {
+        let f = |x: usize| x.pow(x.try_into().unwrap());
+        assert_eq!(f(2), 4)
+    }
+}
