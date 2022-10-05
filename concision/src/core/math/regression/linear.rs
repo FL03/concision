140fa0993e423c4f0b7886e9421a1e8c53ec4d30
Joe McCain III <jo3mccain@scattered-systems.com>
2022-10-05 14:59:37 +0000

    Update

---
+++ concision/src/core/math/regression/linear.rs
@@ -0,0 +1,14 @@
+/*
+    Appellation: linear <module>
+    Contributors: FL03 <jo3mccain@icloud.com> (https://gitlab.com/FL03)
+    Description:
+        ... Summary ...
+*/
+
+pub struct LinearRegression;
+
+impl LinearRegression {
+    pub fn new() -> Self {
+        Self
+    }
+}
---
+++ concision/src/core/math/regression/mod.rs
@@ -0,0 +1,7 @@
+/*
+    Appellation: regression <module>
+    Contributors: FL03 <jo3mccain@icloud.com> (https://gitlab.com/FL03)
+    Description:
+        ... Summary ...
+*/
+
