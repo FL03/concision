5a093c8cc3733f7ab36edd74982cd0b5b98e149c
FL03 <jo3mccain@icloud.com>
2022-12-08 15:42:31 -0600

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- concision/examples/simple.rs
+++ concision/examples/simple.rs
@@ -1,7 +1,15 @@
-use concision as cnc;
+use concision::num::complex::{C, Complex};
+use std::ops::Mul;
+
+// Define a holomorphic function that squares its input.
+fn square<T: Complex + Mul<Output = T> + Clone>(z: T) -> T {
+    z.clone() * z
+}
 
 fn main() {
-    let a = "";
-
-    println!("{:?}", a);
+    let c = C::from((1.0, 1.0));
+    let res = square(c);
+    assert_eq!(res.clone(), C::from((0.0, 2.0)));
+    
+    println!("{:?}", res);
 }
