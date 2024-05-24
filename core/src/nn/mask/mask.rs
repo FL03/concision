ead13404d3cc23d6c4f08c338529a5c03ed6ac10
Joe McCain III <jo3mccain@icloud.com>
2024-05-24 09:23:37 -0500

    update
    Signed-off-by: Joe McCain III <jo3mccain@icloud.com>

--- core/src/nn/mask/mask.rs
+++ core/src/nn/mask/mask.rs
@@ -2,8 +2,9 @@
     Appellation: mask <module>
     Contrib: FL03 <jo3mccain@icloud.com>
 */
+use nd::iter::{Iter, IterMut};
 use nd::prelude::*;
-use nd::{OwnedRepr, RawData};
+use nd::{Data, DataMut, OwnedRepr, RawData, RawDataClone};
 
 pub struct Mask<S = OwnedRepr<bool>, D = Ix2>(ArrayBase<S, D>)
 where
@@ -15,8 +16,97 @@ where
     D: Dimension,
     S: RawData<Elem = bool>,
 {
-    pub fn new(data: ArrayBase<S, D>) -> Self {
+    pub fn from_arr(data: ArrayBase<S, D>) -> Self {
         Self(data)
+    }
+
+    pub fn apply<A, T, F>(&mut self, data: &ArrayBase<T, D>, fill: A) -> ArrayBase<T, D>
+    where
+        A: Clone,
+        S: Data,
+        T: DataMut<Elem = A> + RawDataClone,
+    {
+        let mut res = data.clone();
+        res.zip_mut_with(self.as_mut(), |x, &m| {
+            if m {
+                *x = fill.clone();
+            }
+        });
+        res
+    }
+
+    pub fn mask_inplace<'a, A, T, F>(&mut self, data: &'a mut ArrayBase<T, D>, fill: A) -> &'a mut ArrayBase<T, D>
+    where
+        A: Clone,
+        S: Data,
+        T: DataMut<Elem = A>,
+    {
+        data.zip_mut_with(&mut self.0, |x, &m| {
+            if m {
+                *x = fill.clone();
+            }
+        });
+        data
+    }
+
+    pub fn as_slice(&self) -> &[bool]
+    where
+        S: Data,
+    {
+        self.get().as_slice().unwrap()
+    }
+
+    pub fn as_mut_slice(&mut self) -> &mut [bool]
+    where
+        S: DataMut,
+    {
+        self.get_mut().as_slice_mut().unwrap()
+    }
+
+    pub fn dim(&self) -> D::Pattern {
+        self.get().dim()
+    }
+
+    pub fn iter(&self) -> Iter<'_, bool, D>
+    where
+        S: Data,
+    {
+        self.get().iter()
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_, bool, D>
+    where
+        S: DataMut,
+    {
+        self.get_mut().iter_mut()
+    }
+
+    pub fn get(&self) -> &ArrayBase<S, D> {
+        &self.0
+    }
+
+    pub fn get_mut(&mut self) -> &mut ArrayBase<S, D> {
+        &mut self.0
+    }
+
+    pub fn into_inner(self) -> ArrayBase<S, D> {
+        self.0
+    }
+
+    pub fn ndim(&self) -> usize {
+        self.get().ndim()
+    }
+
+    pub fn raw_dim(&self) -> D {
+        self.get().raw_dim()
+    }
+
+    pub fn set(&mut self, data: ArrayBase<S, D>) {
+        self.0 = data;
+    }
+
+    pub fn shape(&self) -> D {
+        self.get().raw_dim()
     }
 }
 
@@ -26,8 +116,8 @@ where
 mod impls {
     use super::Mask;
     use core::borrow::{Borrow, BorrowMut};
-    use core::ops::{Deref, DerefMut};
-    use nd::{ArrayBase, Dimension, RawData};
+    use core::ops::{Deref, DerefMut, Index, IndexMut};
+    use nd::{ArrayBase, Data, DataMut, Dimension, NdIndex, RawData};
 
     impl<S, D> AsRef<ArrayBase<S, D>> for Mask<S, D>
     where
@@ -88,6 +178,30 @@ mod impls {
     {
         fn deref_mut(&mut self) -> &mut Self::Target {
             &mut self.0
+        }
+    }
+
+    impl<S, D, I> Index<I> for Mask<S, D>
+    where
+        D: Dimension,
+        I: NdIndex<D>,
+        S: Data<Elem = bool>,
+    {
+        type Output = <ArrayBase<S, D> as Index<I>>::Output;
+
+        fn index(&self, index: I) -> &Self::Output {
+            &self.0[index]
+        }
+    }
+
+    impl<S, D, I> IndexMut<I> for Mask<S, D>
+    where
+        D: Dimension,
+        I: NdIndex<D>,
+        S: DataMut<Elem = bool>,
+    {
+        fn index_mut(&mut self, index: I) -> &mut Self::Output {
+            &mut self.0[index]
         }
     }
 }
--- core/src/nn/model.rs
+++ core/src/nn/model.rs
@@ -28,3 +28,32 @@ where
 
     fn context(&self) -> Self::Ctx;
 }
+
+/// This trait describes any neural networks or models that 
+/// adhears to the deep netural network architecture.
+/// This design considers a single input and output layer, while
+/// allowing for any number of hidden layers to be persisted.
+/// 
+/// The `HIDDEN` constant is used to specify the number of hidden layers
+/// and is used to compute the total number of layers (HIDDEN + 2)
+pub trait DeepNeuralNetwork<S, T>: Forward<S, Output = T> {
+    const HIDDEN: Option<usize> = None;
+
+    type Input: Forward<S, Output = T>;
+    type Hidden: Forward<T, Output = T>; // The type of `hidden` layers; all hidden layers implement the same activation function
+    type Out: Forward<T, Output = T>;
+
+    fn input(&self) -> &Self::Input;
+
+    fn hidden(&self) -> &[Self::Hidden];
+
+    fn output(&self) -> &Self::Out;
+
+    fn nlayers(&self) -> usize  {
+        self.nhidden() + 2
+    }
+
+    fn nhidden(&self) -> usize {
+        Self::HIDDEN.unwrap_or_else(|| self.hidden().len())
+    }
+}
