4910f5ecd147d743b168cefc743da051dc4aae26
Joe McCain III <jo3mccain@icloud.com>
2025-01-31 06:58:43 -0600

    update
    Signed-off-by: Joe McCain III <jo3mccain@icloud.com>

--- core/src/func/loss/mod.rs
+++ core/src/func/loss/mod.rs
@@ -2,18 +2,15 @@
     Appellation: loss <module>
     Contrib: FL03 <jo3mccain@icloud.com>
 */
-pub use self::reg::prelude::*;
-pub use self::{entropy::*, utils::*};
-
-pub(crate) mod utils;
+#[doc(inline)]
+pub use self::{entropy::*, reg::*};
 
 pub mod entropy;
 pub mod reg;
 
 pub(crate) mod prelude {
     pub use super::Loss;
-    pub use super::reg::prelude::*;
-    pub use super::utils::*;
+    pub use super::reg::*;
 }
 
 pub trait Loss<A, B = A> {
