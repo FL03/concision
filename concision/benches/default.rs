c4c04308727ef29bf22a2578906da026a06c3fe4
FL03 <jo3mccain@icloud.com>
2025-06-10 09:54:34 -0500

    update benches
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- concision/benches/default.rs
+++ concision/benches/default.rs
@@ -68,6 +68,7 @@ pub mod fib {
 
     /// a simple implementation of the fibonacci sequence for benchmarking purposes
     /// **Warning:** This will overflow the 128-bit unsigned integer at n=186
+    #[inline]
     pub fn fibonacci(n: usize) -> u128 {
         // Use a and b to store the previous two values in the sequence
         let mut a = 0;
@@ -82,8 +83,8 @@ pub mod fib {
         b
     }
     /// a recursive implementation of the fibonacci sequence
-    pub fn recursive_fibonacci(n: usize) -> u128 {
-        fn _inner(n: usize, previous: u128, current: u128) -> u128 {
+    pub const fn recursive_fibonacci(n: usize) -> u128 {
+        const fn _inner(n: usize, previous: u128, current: u128) -> u128 {
             if n == 0 {
                 current
             } else {
@@ -106,31 +107,65 @@ pub mod fib {
         pub fn new() -> Fibonacci {
             Fibonacci { curr: 0, next: 1 }
         }
-
+        /// returns a copy of the current value
+        pub const fn curr(&self) -> u32 {
+            self.curr
+        }
+        /// returns a mutable reference to the current value
+        pub const fn curr_mut(&mut self) -> &mut u32 {
+            &mut self.curr
+        }
+        /// returns a copy of the next value
+        pub const fn next(&self) -> u32 {
+            self.next
+        }
+        /// returns a mutable reference to the next value
+        pub const fn next_mut(&mut self) -> &mut u32 {
+            &mut self.next
+        }
+        /// computes the nth value of the fibonacci sequence
+        #[inline]
         pub fn compute(&mut self, n: usize) -> u32 {
             if let Some(res) = self.nth(n + 1) {
                 return res;
             }
             panic!("Unable to compute the nth value of the fibonacci sequence...")
         }
-
-        pub fn set_curr(&mut self, curr: u32) -> &mut Self {
+        /// reset the instance to its default state, with `curr` set to 0 and `next` set to 1
+        pub const fn reset(&mut self) -> &mut Self {
+            self.set_curr(0).set_next(1)
+        }       
+        /// compute the next value in the fibonacci sequence, using the current and next values
+        #[inline]
+        const fn compute_next(&self) -> u32 {
+            self.curr() + self.next()
+        }
+        /// [`replace`](core::mem::replace) the current value with the given value, returning the 
+        /// previous value
+        const fn replace_curr(&mut self, curr: u32) -> u32 {
+            core::mem::replace(self.curr_mut(), curr)
+        }
+        /// [`replace`](core::mem::replace) the next value with the given value, returning the
+        /// previous value
+        const fn replace_next(&mut self, next: u32) -> u32 {
+            core::mem::replace(self.next_mut(), next)
+        }
+        /// update the current value and return a mutable reference to the instance
+        const fn set_curr(&mut self, curr: u32) -> &mut Self {
             self.curr = curr;
             self
         }
-
-        pub fn set_next(&mut self, next: u32) -> &mut Self {
+        /// update the next value and return a mutable reference to the instance
+        const fn set_next(&mut self, next: u32) -> &mut Self {
             self.next = next;
             self
         }
-
-        pub fn reset(&mut self) -> &mut Self {
-            self.set_curr(0).set_next(1)
-        }
-
-        fn compute_next(&self) -> u32 {
-            self.curr + self.next
-        }
+        /// replace the next value with the given, using the previous next as the new current 
+        /// value, and returning the previous current value
+        const fn update(&mut self, next: u32) -> u32 {
+            let new = self.replace_next(next);
+            self.replace_curr(new)
+        } 
     }
 
     impl Default for Fibonacci {
@@ -143,11 +178,13 @@ pub mod fib {
         type Item = u32;
 
         fn next(&mut self) -> Option<u32> {
-            use core::mem::replace;
+            // compute the new next value
             let new_next = self.compute_next();
-            let new_curr = replace(&mut self.next, new_next);
+            // replace the current next value and use the previous next value as the new 
+            // current value then return the previous current value
+            let prev = self.update(new_next);
 
-            Some(replace(&mut self.curr, new_curr))
+            Some(prev)
         }
     }
 }
