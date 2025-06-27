c46be7d71a053dc6c388b7488245428015bee0ab
FL03 <jo3mccain@icloud.com>
2025-06-26 23:49:21 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- neural/src/model/params.rs
+++ neural/src/model/params.rs
@@ -17,24 +17,30 @@ use crate::RawHidden;
 /// a type alias for an owned representation of the [`DeepParamsBase`] generic of type `A` and
 /// the dimension `D`.
 pub type DeepModelParams<A, D = Ix2> = DeepParamsBase<ndarray::OwnedRepr<A>, D>;
-
+/// a type alias for a _deep_ representation of the [`ModelParamsBase`] using a vector of
+/// parameters as the hidden layers.
 pub type DeepParamsBase<S, D> = ModelParamsBase<S, D, Vec<ParamsBase<S, D>>>;
 
 /// a type alias for an owned representation of the [`DeepParamsBase`] generic of type `A` and
 /// the dimension `D`.
 pub type ShallowModelParams<A, D = Ix2> = ShallowParamsBase<ndarray::OwnedRepr<A>, D>;
-
+/// a type alias for a _shallow_ representation of the [`ModelParamsBase`] using a single
+/// [`ParamsBase`] instance as the hidden layer.
 pub type ShallowParamsBase<S, D> = ModelParamsBase<S, D, ParamsBase<S, D>>;
 
-/// The [`DeepParamsBase`]
+/// The [`ModelParamsBase`] object is a generic ocntainer for storing the parameters of a
+/// neural network, regardless of the layout (e.g. shallow or deep). This is made possible
+/// through the introduction of a generic hidden layer type, `H`, that allows us to define
+/// aliases and additional traits for contraining the hidden layer type. That being said, we
+/// don't reccoment using this type directly, but rather use the provided type aliases such as
+/// [`DeepModelParams`] or [`ShallowModelParams`] or their owned variants. These provide a much
+/// more straighforward interface for typing the parameters of a neural network. We aren't too
+/// worried about the transumtation between the two since users desiring this ability should
+/// simply stick with a _deep_ representation, initializing only a single layer within the
+/// respective container.
 ///
-/// This object is an abstraction over the parameters of a deep neural network model. This is
-/// done to isolate the necessary parameters from the specific logic within a model allowing us
-/// to easily create additional stores for tracking velocities, gradients, and other metrics
-/// we may need.
-///
-/// Additionally, this provides us with a way to introduce common creation routines for
-/// initializing neural networks.
+/// This type also enables us to define a set of common initialization routines and introduce
+/// other standards for dealing with parameters in a neural network.
 pub struct ModelParamsBase<S, D, H>
 where
     D: Dimension,
