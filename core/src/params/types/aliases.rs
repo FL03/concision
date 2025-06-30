94463660002b8b3f84ef54f005a04c9d624923b8
FL03 <jo3mccain@icloud.com>
2025-06-29 23:24:29 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

---
+++ core/src/params/types/aliases.rs
@@ -0,0 +1,24 @@
+/*
+    appellation: aliases <module>
+    authors: @FL03
+*/
+use crate::params::ParamsBase;
+
+use ndarray::{CowRepr, Ix2, OwnedArcRepr, OwnedRepr, RawViewRepr, ViewRepr};
+
+/// A type alias for owned parameters
+pub type Params<A, D = Ix2> = ParamsBase<OwnedRepr<A>, D>;
+/// A type alias for shared parameters
+pub type ArcParams<A, D = Ix2> = ParamsBase<OwnedArcRepr<A>, D>;
+/// A type alias for an immutable view of the parameters
+pub type ParamsView<'a, A, D = Ix2> = ParamsBase<ViewRepr<&'a A>, D>;
+/// A type alias for a mutable view of the parameters
+pub type ParamsViewMut<'a, A, D = Ix2> = ParamsBase<ViewRepr<&'a mut A>, D>;
+/// A type alias for a [`ParamsBase`] with a _borrowed_ internal layout
+pub type CowParams<'a, A, D = Ix2> = ParamsBase<CowRepr<'a, A>, D>;
+/// A type alias for the [`ParamsBase`] whose elements are of type `*const A` using a
+/// [`RawViewRepr`] layout
+pub type RawViewParams<A, D = Ix2> = ParamsBase<RawViewRepr<*const A>, D>;
+/// A type alias for the [`ParamsBase`] whose elements are of type `*mut A` using a
+/// [`RawViewRepr`] layout
+pub type RawMutParams<A, D = Ix2> = ParamsBase<RawViewRepr<*mut A>, D>;
--- tensor/src/types/aliases.rs
+++ tensor/src/types/aliases.rs
@@ -3,7 +3,7 @@
     authors: @FL03
 */
 use crate::tensor::TensorBase;
-use ndarray::{Ix2, OwnedArcRepr, OwnedRepr, RawViewRepr, ViewRepr};
+use ndarray::{CowRepr, Ix2, OwnedArcRepr, OwnedRepr, RawViewRepr, ViewRepr};
 
 /// a type alias for a [`TensorBase`] with an owned representation
 pub type Tensor<A, D = Ix2> = TensorBase<OwnedRepr<A>, D>;
@@ -12,8 +12,11 @@ pub type TensorView<'a, A, D = Ix2> = TensorBase<ViewRepr<&'a A>, D>;
 /// a type alias for a [`TensorBase`] with a mutable view representation
 pub type TensorViewMut<'a, A, D = Ix2> = TensorBase<ViewRepr<&'a mut A>, D>;
 
-/// a type alias for a [`TensorBase`] with an owned representation
+/// a type alias for a [`TensorBase`] setup to use a shared, thread-safe internal 
+/// representation of the data.
 pub type ArcTensor<A, D = Ix2> = TensorBase<OwnedArcRepr<A>, D>;
+/// a type alias for a [`TensorBase`] using a _borrowed_ layout
+pub type CowTensor<'a, A, D = Ix2> = TensorBase<CowRepr<'a, A>, D>;
 /// A type alias for a [`TensorBase`] with a raw pointer representation.
 pub type RawViewTensor<A, D = Ix2> = TensorBase<RawViewRepr<*const A>, D>;
 /// a type alias for a [`TensorBase`] with an owned representation
