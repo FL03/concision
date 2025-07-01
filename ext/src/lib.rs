dfda87b0e80408513be9f96b092f9697cceb942e
FL03 <jo3mccain@icloud.com>
2025-07-01 09:31:44 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- ext/src/lib.rs
+++ ext/src/lib.rs
@@ -1,11 +1,15 @@
 /*
-    Appellation: concision-models <library>
+    Appellation: concision-ext <library>
     Contrib: @FL03
 */
-//! # concision-ext
+//! An extension of the [`concision`](https://crates.io/crates/concision) library, focusing on
+//! providing additional layers and other non-essential features for building more complex
+//! neural networks and machine learning models.
 //!
-//! This library uses the [`concision`](https://docs.rs/concision) framework to implement a
-//! variety of additional machine learning models and layers.
+//! ## Features
+//!
+//! - **attention**: Enables various attention mechanisms from scaled dot-product and
+//!   multi-headed attention to FFT-based attention.
 //!
 #![allow(clippy::module_inception, clippy::needless_doctest_main)]
 #![cfg_attr(not(feature = "std"), no_std)]
@@ -21,7 +25,7 @@ pub use self::attention::prelude::*;
 #[cfg(feature = "attention")]
 pub mod attention;
 
-pub mod simple;
+// pub mod simple;
 
 pub mod prelude {
     #[cfg(feature = "attention")]
