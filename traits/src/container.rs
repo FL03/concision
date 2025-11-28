d36b1a71e6dca435a93cd36075f9ed4ec5e2479e
FL03 <jo3mccain@icloud.com>
2025-11-28 15:48:48 -0600

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- traits/src/container.rs
+++ traits/src/container.rs
@@ -3,14 +3,59 @@
     Created At: 2025.11.28:15:30:46
     Contrib: @FL03
 */
-
+/// The [`Container`] trait defines a generic interface for container types.
 pub trait Container {
-    type Cont<U>;
+    type Cont<U>: ?Sized;
     type Item;
-
 }
 
-impl<X> Container for Option<X> {
-    type Cont<U> = Option<U>;
-    type Item = X;
+macro_rules! container {
+    ($(
+        $($container:ident)::*<$A:ident $(, $B:ident)?>
+    ),* $(,)?) => {
+        $(container!(@impl $($container)::*<$A $(, $B)?>);)*
+    };
+
+    (@impl $($container:ident)::*<$T:ident>) => {
+        impl<$T> $crate::container::Container for $($container)::*<$T> {
+            type Cont<U> = $($container)::*<U>;
+            type Item = $T;
+        }
+    };
+    (@impl $($container:ident)::*<$K:ident, $V:ident>) => {
+        impl<$K, $V>  $crate::container::Container for $($container)::*<$K, $V> {
+            type Cont<U> = $($container)::*<$K, U>;
+            type Item = $V;
+        }
+    };
+}
+
+impl<T> Container for [T] {
+    type Cont<U> = [U];
+    type Item = T;
+}
+
+container! {
+    core::option::Option<T>,
+    core::result::Result<T, E>,
+}
+
+#[cfg(feature = "alloc")]
+container! {
+    alloc::boxed::Box<T>,
+    alloc::vec::Vec<T>,
+    alloc::collections::BTreeMap<K, V>,
+    alloc::collections::BTreeSet<K>,
+    alloc::collections::VecDeque<T>,
+    alloc::rc::Rc<T>,
+    alloc::sync::Arc<T>,
+}
+
+#[cfg(feature = "std")]
+container! {
+    std::collections::HashMap<K, V>,
+    std::collections::HashSet<K>,
+    std::cell::Cell<T>,
+    std::sync::Mutex<T>,
+    std::sync::RwLock<T>,
 }
