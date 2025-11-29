2d08989f65e5323e6e3eb808cd96c8979ee978e4
FL03 <jo3mccain@icloud.com>
2025-11-29 00:01:41 -0600

    update tests
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- params/tests/propagate.rs
+++ params/tests/propagate.rs
@@ -4,17 +4,17 @@
 */
 use concision_params::Params;
 
-use approx::assert_abs_diff_eq;
-use ndarray::prelude::*;
+use ndarray::array;
 
+/*
+    Verify the dimensionality of the output of forward propagation
+*/
 #[test]
-fn test_params_forward() {
+fn test_params_fwd_dimensionality() {
     let params = Params::<f64>::ones((3, 4));
     let input = array![1.0, 2.0, 3.0];
-    // should be of shape 4: (d_in, d_out).t() * (d_in,) + (d_out,)
+    // should be of shape 4:
     let output = params.forward(&input);
-    assert_eq!(output.dim(), 4);
-    // output should be: $W.t() * x + b = [7.0, 7.0, 7.0, 7.0]$
-    // where W = ones(3, 4) and b = ones(4)
-    assert_abs_diff_eq!(output, array![7.0, 7.0, 7.0, 7.0], epsilon = 1e-3);
+    assert_eq!(output.dim(), 4); // (in, out).t() * (in,) + (out,)
+    assert_eq!(output, array![7.0, 7.0, 7.0, 7.0]); // w.t() * x + b = [7.0, 7.0, 7.0, 7.0]
 }
