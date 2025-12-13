f61a2f6f313359d4c54d8257c7313db49da5fc1f
FL03 <jo3mccain@icloud.com>
2025-12-13 08:46:22 -0600

    update hkt.rs
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- traits/src/hkt.rs
+++ traits/src/hkt.rs
@@ -5,43 +5,75 @@
 
 /// The [`HKT`] trait defines an interface for higher-kinded types (HKT).
 pub trait HKT<T> {
-    type Cont<_T>;
+    type Cont<_T>: ?Sized;
 }
 
 /// The [`Functor`] trait extends the [`HKT`] trait to provide a way to map over its content(s)
 /// using a function `f` that transforms values of type `T` into values of type `U`.
 pub trait Functor<T>: HKT<T> {
+    fn mapf<F, U>(self, f: F) -> Self::Cont<U>
+    where
+        F: Fn(T) -> U;
+}
+/// An alternative version of the [`Functor`] trait that works with references to the content
+pub trait FunctorRef<T>: HKT<T> {
     fn mapf<F, U>(&self, f: F) -> Self::Cont<U>
     where
         F: Fn(&T) -> U;
 }
 
-pub trait Applicative<T>: Functor<T> {
-    fn pure(value: T) -> Self::Cont<T>;
-}
+// pub trait Applicative<T>: Functor<T> {
+//     fn pure(value: T) -> Self::Cont<T>;
+// }
+
+// pub trait Monad<T>: Applicative<T> {
+//     fn flat_map<F, U>(&self, f: F) -> Self::Cont<U>
+//     where
+//         F: Fn(&mut T, &T) -> Self::Cont<U>;
+// }
 
 /*
  *************  Implementations  *************
 */
 macro_rules! hkt {
-    (@impl $($t:ident)::*<$T:ident>) => {
-        impl<$T> HKT<$T> for $($t)::*<$T> {
-            type Cont<_T> = $($t)::*<_T>;
+    (@impl $($cont:ident)::*<$T:ident>) => {
+        impl<$T> HKT<$T> for $($cont)::*<$T> {
+            type Cont<_T> = $($cont)::*<_T>;
         }
     };
-    (
-        $(
-            $($t:ident)::*<$T:ident>
-        ),* $(,)?
-    ) => {
-        $(
-            hkt!(@impl $($t)::*<$T>);
-        )*
+    ($($($cont:ident)::*<$T:ident>),* $(,)?) => {
+        $(hkt!(@impl $($cont)::*<$T>);)*
     };
 }
 
+impl<C, T> HKT<T> for &C
+where
+    C: HKT<T>,
+{
+    type Cont<U> = C::Cont<U>;
+}
+
+impl<C, T> HKT<T> for &mut C
+where
+    C: HKT<T>,
+{
+    type Cont<U> = C::Cont<U>;
+}
+
+impl<T> HKT<T> for [T] {
+    type Cont<U> = [U];
+}
+
+impl<const N: usize, T> HKT<T> for [T; N] {
+    type Cont<U> = [U; N];
+}
+
+impl<T, E> HKT<T> for core::result::Result<T, E> {
+    type Cont<U> = core::result::Result<U, E>;
+}
+
 hkt! {
-    core::option::Option<T>
+    core::option::Option<T>,
 }
 
 #[cfg(feature = "alloc")]
@@ -78,7 +110,34 @@ impl<K, V> HKT<V> for std::collections::HashMap<K, V> {
     type Cont<U> = std::collections::HashMap<K, U>;
 }
 
+#[cfg(feature = "hashbrown")]
+impl<K, V, S> HKT<V> for hashbrown::HashMap<K, V, S> {
+    type Cont<U> = hashbrown::HashMap<K, U, S>;
+}
+
+#[cfg(feature = "hashbrown")]
+impl<T, S> HKT<T> for hashbrown::HashSet<T, S> {
+    type Cont<U> = hashbrown::HashSet<U, S>;
+}
+
+impl<A, S, D> HKT<A> for ndarray::ArrayBase<S, D, A>
+where
+    S: ndarray::RawData<Elem = A>,
+    D: ndarray::Dimension,
+{
+    type Cont<U> = ndarray::ArrayRef<U, D>;
+}
+
 impl<T> Functor<T> for Option<T> {
+    fn mapf<F, U>(self, f: F) -> Self::Cont<U>
+    where
+        F: Fn(T) -> U,
+    {
+        self.map(f)
+    }
+}
+
+impl<T> FunctorRef<T> for Option<T> {
     fn mapf<F, U>(&self, f: F) -> Self::Cont<U>
     where
         F: Fn(&T) -> U,
@@ -94,6 +153,6 @@ mod tests {
     #[test]
     fn test_option() {
         let opt = Some(42u8);
-        assert_eq!(opt.mapf(|&x| x as f32 + 1.25), Some(43.25));
+        assert_eq!(opt.mapf(|x| x as f32 + 1.25), Some(43.25));
     }
 }
